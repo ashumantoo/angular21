7e61240af754aea634fc163ac0ae798d9ba72348
Ashutosh Kumar Mantoo <ashumantoo@gmail.com>
2025-12-28 13:07:10 +0530

    control flow and bootstrap

--- src/app/components/control-flow/control-flow.ts
+++ src/app/components/control-flow/control-flow.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-flow.html',
   styleUrl: './control-flow.css',
 })
 export class ControlFlow {
+  isParaVisible = true;
 
+  monthStartsName = signal("Dec");
+
+  cityNames = ["Bangalore", "Delhi", "Noida", "Pune", "Mumbai"];
+
+  playerLists = [
+    { name: "Virat Kohli", age: 38, phoneNumber: "887898999", isActive: true },
+    { name: "Sachin", age: 49, phoneNumber: "022-435333", isActive: false },
+    { name: "Rohit Sharma", age: 39, phoneNumber: "022-55444", isActive: true },
+    { name: "Rahul Dravid", age: 45, phoneNumber: "022-435333", isActive: false },
+    { name: "Jasprit Bumrah", age: 33, phoneNumber: "022-43333", isActive: true }
+  ]
+
+  onShowPara() {
+    this.isParaVisible = true;
+  }
+
+  onHidePara() {
+    this.isParaVisible = false;
+  }
 }
